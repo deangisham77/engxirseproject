1d4aacff96d69e7eec98a22695d2960333010040
irsemoticon9 <88526502+irsemoticon9@users.noreply.github.com>
2026-07-20 14:53:24 +0700

    Create Qentury.lua

---
+++ Qentury.lua
@@ -0,0 +1,18 @@
+local scripts = {
+    [129827112113663] = "https://github.com/deangisham77/engxirseproject/raw/refs/heads/main/EngIRSE.lua",
+    [111896378748580] = "https://raw.githubusercontent.com/irsemoticon9/QenturyHub/refs/heads/main/kerangajaib.lua",
+	  [117623186846996] = "https://github.com/deangisham77/engxirseproject/raw/refs/heads/main/EngIRSE.lua",
+    [86111605798689] = "https://raw.githubusercontent.com/irsemoticon9/QenturyHub/refs/heads/main/starfishing.lua",
+    [90457367396205] = "https://raw.githubusercontent.com/deangisham77/engxirseproject/refs/heads/main/fishandmonster.lua",
+    [111385005478215] = "https://raw.githubusercontent.com/deangisham77/engxirseproject/refs/heads/main/fishandmonster.lua",
+    [125927821145949] = "https://raw.githubusercontent.com/deangisham77/engxirseproject/refs/heads/main/budakfreeport.lua",
+    [98800969324557] = "https://raw.githubusercontent.com/irsemoticon9/QenturyHub/refs/heads/main/Storagehunter.lua",
+}
+
+local url = scripts[game.PlaceId]
+
+if url then
+    loadstring(game:HttpGet(url))()
+else
+    warn(("Unsupported PlaceId: %s"):format(game.PlaceId))
+end
